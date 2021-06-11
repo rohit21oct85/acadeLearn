ddd5ef954a4fb2cf73a45642a1ec61b5aa604600
Sami Ullah <muftisami2010gmail.com>
2021-06-11 19:06:52 +0530

    added mapping and units

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,13 @@
-const baseUrl = "example.com:3000"
-
 let server_type= process.env.SERVER_TYPE;
 
 let apiUrl='';
+let baseUrl = '';
 if(process.env.NODE_ENV === 'development'){
     apiUrl = process.env.REACT_APP_LOCAL_API_URL ;
+    baseUrl = "example.com:3000"
 }else{
     apiUrl = process.env.REACT_APP_LIVE_API_URL;
+    baseUrl = "acadelearn.com"
 }
 
 
